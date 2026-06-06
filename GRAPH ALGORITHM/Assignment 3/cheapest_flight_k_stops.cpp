e3bcae9f46f8da401651a377dc7f783ccbabcea6
Radiyah Faten <radiyahfaten@gmail.com>
2026-06-06 11:35:12 +0600

    Add function to find cheapest flight with up to k stops, update Prim's algorithm code

---
+++ GRAPH ALGORITHM/Assignment 3/cheapest_flight_k_stops.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
+{
+
+    vector<int> dist(n, INT_MAX / 2);
+    dist[src] = 0;
+
+    for (int i = 1; i <= k + 1; i++)
+    {
+
+        vector<int> temp = dist;
+
+        for (auto flight : flights)
+        {
+            int u = flight[0];
+            int v = flight[1];
+            int w = flight[2];
+
+            if (temp[v] > dist[u] + w)
+            {
+                temp[v] = dist[u] + w;
+            }
+        }
+
+        dist = temp;
+    }
+
+    if (dist[dst] == INT_MAX / 2)
+        return -1;
+
+    return dist[dst];
+}
