d82ab8e5f3ac76464897ebf38004c63a2159fea4
Radiyah Faten <radiyahfaten@gmail.com>
2026-06-12 22:18:17 +0600

    max non-adjacent sum

---
+++ DYNAMIC PROGRAMMING/Assignment/ques9.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maximumNonAdjacentSum(vector<int> &arr)
+{
+    int i = 0;
+    int j = 0;
+    int temp = 0;
+    for (int n : arr)
+    {
+        temp = max(i + n, j);
+        i = j;
+        j = temp;
+    }
+    return j;
+}
+
+int main()
+{
+    int T; // test cases
+    cin >> T;
+    while (T)
+    {
+        T--;
+
+        int N;
+        cin >> N;
+
+        vector<int> arr(N);
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i]; // input each element
+        }
+
+        cout << maximumNonAdjacentSum(arr) << endl; // output max sum for this test case
+    }
+    return 0;
